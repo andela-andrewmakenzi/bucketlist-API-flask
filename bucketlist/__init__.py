0c120c571b3ee27fb252365639b3719dd7d28048
andela-andrewmakenzi <andrew.makenzi@andela.com>
2017-01-13 16:21:43 +0300

    Add model import to interact with views

--- bucketlist/__init__.py
+++ bucketlist/__init__.py
@@ -1,6 +1,8 @@
 from flask import Flask
 
-app = Flask(__name__)
-app.secret_key = "A0Zr98j/3yX R~XHH!jmN]LWX/,?RT"
 
+app = Flask(__name__)
+app.config["SECRET_KEY"] = "A0Zr98j/3yX R~XHH!jmN]LWX/,?RT"
+
+from . import model
 from . import views
