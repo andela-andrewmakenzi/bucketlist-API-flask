9e0cd6e22614ffbde615ae012adc871e48357f60
andela-andrewmakenzi <andrew.makenzi@andela.com>
2017-01-13 15:47:08 +0300

    Started project

---
+++ bucketlist/views.py
@@ -0,0 +1,48 @@
+from flask import render_template, request
+from . import app
+
+
+@app.route("/auth/login", methods=["POST"])
+def login():
+    pass
+
+
+@app.route("/auth/register", methods=["POST"])
+def register():
+    pass
+
+
+@app.route("/bucketlists", methods=["GET"])
+def show_bucketlist():
+    pass
+
+
+@app.route("/bucketlists/<id>", methods=["GET"])
+def show_bucketlists():
+    pass
+
+
+@app.route("/bucketlists/<id>", methods=["PUT"])
+def update_bucketlist():
+    pass
+
+
+@app.route("/bucketlists/<id>", methods=["DELETE"])
+def create_bucketlist():
+    pass
+
+
+@app.route("/bucketlists/<id>/items/", methods=["POST"])
+def create_new_item():
+    pass
+
+
+@app.route("/bucketlists/<id>/items/<item_id>", methods=["PUT"])
+def update_bucket_list_item():
+    pass
+
+
+@app.route("/bucketlists/<id>/items/<item_id>", methods=["DELETE"])
+def delete_bucket_list_item():
+    pass
+
