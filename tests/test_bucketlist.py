a2a1a202ca30b6f4991921f1d394b20625fd9a07
andela-andrewmakenzi <andrew.makenzi@andela.com>
2017-01-17 15:17:57 +0300

    Add setup and tear down functions

--- tests/test_bucketlist.py
+++ tests/test_bucketlist.py
@@ -1,5 +1,19 @@
 import unittest
+from bucketlist import app
+import tempfile
+import os
 
 
 class TestBucketList(unittest.TestCase):
-    pass
+    def SetUp(self):
+        self.db_handle, app.config["SQLALCHEMY_DATABASE_URI"] = tempfile.mkstemp()
+        app.config["TESTING"] = True
+        self.app = app.test_client()
+
+    def tearDown(self):
+        os.close(self.db_handle)
+        os.unlink(app.config["SQLALCHEMY_DATABASE_URI"])
+
+
+if __name__ == "__main__":
+    unittest.main()
