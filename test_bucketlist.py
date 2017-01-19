745400957491041963ae6d52aaac940f0681a9d2
andela-andrewmakenzi <andrew.makenzi@andela.com>
2017-01-19 15:07:17 +0300

    Correct on Shem adivce, appropriate status codes, also check returning json on error

--- test_bucketlist.py
+++ test_bucketlist.py
@@ -19,55 +19,76 @@ class TestBucketList(unittest.TestCase):
         db.session.remove()  # will call session remove
         db.drop_all()
 
-    def test_index_page(self):
-        response = self.client.get("/")
-        self.assertEqual(response.status_code, 200)
+    def test_access_invalid_endpoint(self):
+        response = self.client.post("/")
+        self.assertEqual(response.status_code, 404)
+        self.assertTrue(json.loads(response.data))  # if we don't generate JSON here, we will error
 
     def test_login(self):
         # attempt to create new valid user and login
         credentials = {"username": "admin", "password": "admin"}
         response = self.client.post("/auth/login", data=json.dumps(credentials), content_type="application/json")
-        self.assertEqual(response.status_code, 200)
+        self.assertTrue(response.status_code, 200)
 
     def test_login_no_username(self):
         credentials = {"username": "", "password": "admin"}
         response = self.client.post("/auth/login", data=json.dumps(credentials), content_type="application/json")
         self.assertEqual(response.status_code, 401)
+        self.assertTrue(json.loads(response.data))  # test return JSON err msg
 
     def test_login_no_password(self):
         credentials = {"username": "admin", "password": ""}
         response = self.client.post("/auth/login", data=json.dumps(credentials), content_type="application/json")
         self.assertEqual(response.status_code, 401)
+        self.assertTrue(json.loads(response.data))  # test return JSON err msg
 
     def test_login_no_credentials(self):
         credentials = {"username": "", "password": ""}
         response = self.client.post("/auth/login", data=json.dumps(credentials), content_type="application/json")
         self.assertEqual(response.status_code, 401)
+        self.assertTrue(json.loads(response.data))  # test return JSON err msg
+
+    def test_login_no_required_field(self):
+        credentials = {"username": "andrew"}
+        response = self.client.post("/auth/login", data=json.dumps(credentials), content_type="application/json")
+        self.assertEqual(response.status_code, 401)
+        self.assertTrue(json.loads(response.data))  # test return JSON err msg
 
     def test_register_user(self):
         credentials = {"username": "andrew", "password": "andrew"}  # this should be the second user we are registering
         response = self.client.post("/auth/register", data=json.dumps(credentials), content_type="application/json")
-        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.status_code, 201)
 
     def test_register_user_no_username(self):
         credentials = {"username": "", "password": "andrew"}  # this should be the second user we are registering
         response = self.client.post("/auth/register", data=json.dumps(credentials), content_type="application/json")
         self.assertEqual(response.status_code, 401)
+        self.assertTrue(json.loads(response.data))  # test return JSON err msg
 
     def test_register_user_pass_no_password(self):
         credentials = {"username": "andrew", "password": ""}  # this should be the second user we are registering
         response = self.client.post("/auth/register", data=json.dumps(credentials), content_type="application/json")
         self.assertEqual(response.status_code, 401)
+        self.assertTrue(json.loads(response.data))
 
     def test_register_user_pass_no_username_or_password(self):
         credentials = {"username": "", "password": ""}  # this should be the second user we are registering
         response = self.client.post("/auth/register", data=json.dumps(credentials), content_type="application/json")
         self.assertEqual(response.status_code, 401)
+        self.assertTrue(json.loads(response.data))  # test return JSON err msg
 
     def test_register_with_existing_username(self):
         credentials = {"username": "admin", "password": "admin"}  # this should be the second user we are registering
         response = self.client.post("/auth/register", data=json.dumps(credentials), content_type="application/json")
         self.assertEqual(response.status_code, 401)
+        self.assertTrue(json.loads(response.data))  # test return JSON err msg
+
+    def test_register_user_required_field_not_passed(self):
+        # if the dev forgets to add fields in required format as API requires
+        credentials = {"username": "admin"}  # this should be the second user we are registering
+        response = self.client.post("/auth/register", data=json.dumps(credentials), content_type="application/json")
+        self.assertEqual(response.status_code, 401)
+        self.assertTrue(json.loads(response.data))  # test return JSON err msg
 
     def test_create_bucketlist(self):
         user = db.session.query(User).filter_by(username="admin").first()
@@ -75,7 +96,7 @@ class TestBucketList(unittest.TestCase):
         credentials = str(token)
         bucketname = "games to buy"
         response = self.client.post("/bucketlists", data=json.dumps({"token": credentials, "name": bucketname}), content_type="application/json")
-        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.status_code, 201)  # we get this on successful creation
         # also check if there is bucketlist in the db with that name
         name = db.session.query(Bucketlist).filter_by(name=bucketname).first()
         self.assertTrue(name is not None)
@@ -84,7 +105,7 @@ class TestBucketList(unittest.TestCase):
         user = db.session.query(User).filter_by(username="admin").first()
         token = user.generate_auth_token()  # simulate login
         credentials = str(token)
-        bucketname = ""  # blank and invalid name 
+        bucketname = ""  # blank and invalid name
         response = self.client.post("/bucketlists", data=json.dumps({"token": credentials, "name": bucketname}), content_type="application/json")
         self.assertEqual(response.status_code, 401)  # must supply a name for the bucketlist
 
@@ -142,6 +163,11 @@ class TestBucketList(unittest.TestCase):
     # def test_delete_bucketlist_item_unauthorized(self):
     #     pass
 
+    # def test_access_expired_token():
+    #     pass
+
+    # def test_access_invalid_token():
+    #     pass
 
 if __name__ == "__main__":
     unittest.main()
