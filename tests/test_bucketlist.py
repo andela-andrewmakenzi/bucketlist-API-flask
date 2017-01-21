e7fee9eb428be90f6dbdf9d80c69f5a60fa7a40f
andela-andrewmakenzi <andrew.makenzi@andela.com>
2017-01-21 23:38:54 +0300

    Added login user function avoid repeating code

--- tests/test_bucketlist.py
+++ tests/test_bucketlist.py
@@ -1,87 +1,16 @@
-import unittest
-from bucketlist import app
+from .test_base import BaseTestCase
 from bucketlist.model import db, User, Bucketlist
 from flask import json
-from .test_base import BaseTestCase
 
 
 class TestBucketList(BaseTestCase):
 
-    def test_access_invalid_endpoint(self):
-        response = self.client.post("/")
-        self.assertEqual(response.status_code, 404)
-        self.assertTrue(json.loads(response.data))  # if we don't generate JSON here, we will error
-
-    def test_login(self):
-        # attempt to create new valid user and login
-        credentials = {"username": "admin", "password": "admin"}
-        response = self.client.post("/auth/login", data=json.dumps(credentials), content_type="application/json")
-        self.assertTrue(response.status_code, 200)
-
-    def test_login_no_username(self):
-        credentials = {"username": "", "password": "admin"}
-        response = self.client.post("/auth/login", data=json.dumps(credentials), content_type="application/json")
-        self.assertEqual(response.status_code, 401)
-        self.assertTrue(json.loads(response.data))  # test return JSON err msg
-
-    def test_login_no_password(self):
-        credentials = {"username": "admin", "password": ""}
-        response = self.client.post("/auth/login", data=json.dumps(credentials), content_type="application/json")
-        self.assertEqual(response.status_code, 401)
-        self.assertTrue(json.loads(response.data))  # test return JSON err msg
-
-    def test_login_no_credentials(self):
-        credentials = {"username": "", "password": ""}
-        response = self.client.post("/auth/login", data=json.dumps(credentials), content_type="application/json")
-        self.assertEqual(response.status_code, 401)
-        self.assertTrue(json.loads(response.data))  # test return JSON err msg
-
-    def test_login_no_required_field(self):
-        credentials = {"username": "andrew"}
-        response = self.client.post("/auth/login", data=json.dumps(credentials), content_type="application/json")
-        self.assertEqual(response.status_code, 401)
-        self.assertTrue(json.loads(response.data))  # test return JSON err msg
-
-    def test_register_user(self):
-        credentials = {"username": "andrew", "password": "andrew"}  # this should be the second user we are registering
-        response = self.client.post("/auth/register", data=json.dumps(credentials), content_type="application/json")
-        self.assertEqual(response.status_code, 201)
-
-    def test_register_user_no_username(self):
-        credentials = {"username": "", "password": "andrew"}  # this should be the second user we are registering
-        response = self.client.post("/auth/register", data=json.dumps(credentials), content_type="application/json")
-        self.assertEqual(response.status_code, 401)
-        self.assertTrue(json.loads(response.data))  # test return JSON err msg
-
-    def test_register_user_pass_no_password(self):
-        credentials = {"username": "andrew", "password": ""}  # this should be the second user we are registering
-        response = self.client.post("/auth/register", data=json.dumps(credentials), content_type="application/json")
-        self.assertEqual(response.status_code, 401)
-        self.assertTrue(json.loads(response.data))
-
-    def test_register_user_pass_no_username_or_password(self):
-        credentials = {"username": "", "password": ""}  # this should be the second user we are registering
-        response = self.client.post("/auth/register", data=json.dumps(credentials), content_type="application/json")
-        self.assertEqual(response.status_code, 401)
-        self.assertTrue(json.loads(response.data))  # test return JSON err msg
-
-    def test_register_with_existing_username(self):
-        credentials = {"username": "admin", "password": "admin"}  # this should be the second user we are registering
-        response = self.client.post("/auth/register", data=json.dumps(credentials), content_type="application/json")
-        self.assertEqual(response.status_code, 401)
-        self.assertTrue(json.loads(response.data))  # test return JSON err msg
-
-    def test_register_user_required_field_not_passed(self):
-        # if the dev forgets to add fields in required format as API requires
-        credentials = {"username": "admin"}  # this should be the second user we are registering
-        response = self.client.post("/auth/register", data=json.dumps(credentials), content_type="application/json")
-        self.assertEqual(response.status_code, 401)
-        self.assertTrue(json.loads(response.data))  # test return JSON err msg
+    def login_user():
+        user = db.session.query(User).filter_by(username="admin").first()
+        token = user.generate_auth_token().decode("utf-8")  # simulate login       
 
     def test_create_bucketlist(self):
-        user = db.session.query(User).filter_by(username="admin").first()
-        # token = str(user.generate_auth_token())  # simulate login
-        token = user.generate_auth_token().decode('utf-8')
+        self.login_user()
         bucketname = "games to buy"
         response = self.client.post("/bucketlists", data=json.dumps({"name": bucketname}), headers={"Authorization": "Bearer {}".format(token)})
         self.assertEqual(response.status_code, 201)  # we get this on successful creation
@@ -90,75 +19,82 @@ class TestBucketList(BaseTestCase):
         self.assertTrue(name is not None)
 
     def test_create_bucketlist_no_bucketlistname(self):
-        user = db.session.query(User).filter_by(username="admin").first()
-        token = user.generate_auth_token().decode("utf-8")  # simulate login
+        self.login_user()
         bucketname = ""  # blank and invalid name
         response = self.client.post("/bucketlists", data=json.dumps({"name": bucketname}), headers={"Authorization": "Bearer {}".format(token)})
         self.assertEqual(response.status_code, 401)  # must supply a name for the bucketlist
 
     def test_get_bucketlists(self):
-        user = db.session.query(User).filter_by(username="admin").first()
-        token = user.generate_auth_token().decode("utf-8")  # simulate login
-        bucketname = ""  # blank and invalid name
-        response = self.client.post("/bucketlists", data=json.dumps({"name": bucketname}), headers={"Authorization": "Bearer {}".format(token)})
-        self.assertEqual(response.status_code, 401)  # must supply a name for the bucketlists
+        self.login_user()
+        response = self.client.get("/bucketlists", headers={"Authorization": "Bearer {}".format(token)})
+        self.assertEqual(response.status_code, 200)
+        self.assertTrue(type(response.data) == "json")
 
-    # def test_get_bucketlists_invalid_id(self):
-    #     pass
+    def test_get_bucketlists_with_id(self):
+        self.login_user()
+        response = self.client.get("/bucketlists<{}>".format(user.id), headers={"Authorization": "Bearer {}".format(token)})
+        self.assertEqual(response.status_code, 200)
+        self.assertTrue(type(response.data) == "json")
 
-    # def test_get_bucketlist_unauthorized(self):
-    #     pass
+    def test_get_bucketlists_invalid_id(self):
+        self.login_user()
+        response = self.client.get("/bucketlists<{}>".format(user.id), headers={"Authorization": "Bearer {}".format(token)})
+        self.assertEqual(response.status_code, 200)
+        self.assertTrue(type(response.data) == "json")
 
-    # def tests_update_bucketlist(self):
-    #     pass
+    def test_get_bucketlist_unauthorized(self):
+        pass
 
-    # def test_update_bucketlist_invalid_id(self):
-    #     pass
+    def tests_update_bucketlist(self):
+        pass
 
-    # def test_update_bucket_unauthorized(self):
-    #     pass
+    def test_update_bucketlist_invalid_id(self):
+        pass
 
-    # def test_delete_bucketlist(self):
-    #     pass
+    def test_update_bucket_unauthorized(self):
+        pass
 
-    # def test_delete_bucketlist_invalid_id(self):
-    #     pass
+    def test_delete_bucketlist(self):
+        pass
 
-    # def test_delete_bucketlist_unauthorized(self):
-    #     pass
+    def test_delete_bucketlist_invalid_id(self):
+        pass
 
-    # def test_create_new_item_bucketlist(self):
-    #     pass
+    def test_delete_bucketlist_unauthorized(self):
+        pass
 
-    # def test_create_bucketlist_item_no_name(self):
-    #     pass
+    def test_create_new_item_bucketlist(self):
+        pass
 
-    # def test_create_bucketlist_item_unauthorized(self):
-    #     pass
+    def test_create_bucketlist_item_no_name(self):
+        pass
 
-    # def test_update_item_bucketlist(self):
-    #     pass
+    def test_create_bucketlist_item_unauthorized(self):
+        pass
 
-    # def test_update_bucketlist_item_invalid_id(self):
-    #     pass
+    def test_update_item_bucketlist(self):
+        pass
 
-    # def test_update_bucketlist_unauthorized(self):
-    #     pass
+    def test_update_bucketlist_item_invalid_id(self):
+        pass
 
-    # def test_delete_item_bucketlist(self):
-    #     pass
+    def test_update_bucketlist_unauthorized(self):
+        pass
 
-    # def test_delete_bucketlist_item_invalid_id(self):
-    #     pass
+    def test_delete_item_bucketlist(self):
+        pass
 
-    # def test_delete_bucketlist_item_unauthorized(self):
-    #     pass
+    def test_delete_bucketlist_item_invalid_id(self):
+        pass
 
-    # def test_access_expired_token():
-    #     pass
+    def test_delete_bucketlist_item_unauthorized(self):
+        pass
 
-    # def test_access_invalid_token():
-    #     pass
+    def test_access_expired_token():
+        pass
+
+    def test_access_invalid_token():
+        pass
 
 
 if __name__ == "__main__":
